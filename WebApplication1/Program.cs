fff72c290c7ff573ba799d2622e0f07efa59c6e8
Maksymilian Byrdziak <maksymilian.byrdziak@gmail.com>
2025-11-17 15:11:37 +0100

    Update Program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+// 1. Mówimy, że będziemy używać podstawowych narzędzi (jak 'Console')
+using System;
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+// 2. Tworzymy "pudło" na nasz program
+class Program
+{
+    // 3. Tworzymy główny "punkt startowy" programu
+    public static void Main(string[] args)
+    {
+    Console.WriteLine($"Ile masz lat?");
+    string wiek = Console.ReadLine()??"";
+    int wiek1 = Convert.ToInt32(wiek);
+    int wiekza5 = wiek1 + 5;
+    Console.WriteLine($"Za 5 lat będziesz mieć {wiekza5}");
+    }
+}
